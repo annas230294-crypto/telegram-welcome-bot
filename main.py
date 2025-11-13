f80ad51294fd5fd5150f741d9e8eb35ff52421be
annas230294-crypto <annas230294@gmail.com>
2025-11-13 14:43:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from flask import Flask
+from telegram.ext import Application, CommandHandler
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+app = Flask(__name__)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот работает! ✅")
+@app.route('/')
+def home():
+    return "Bot is running! ✅"
 
-app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def start(update, context):
+    update.message.reply_text("Бот работает! ✅")
 
-print("Бот запущен!")
-app.run_polling()
+if __name__ == "__main__":
+    bot = Application.builder().token(BOT_TOKEN).build()
+    bot.add_handler(CommandHandler("start", start))
+    bot.run_polling()
+    app.run(host='0.0.0.0', port=5000)
